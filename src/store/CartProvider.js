d32284dda3deddc3a9b7cab0718cb2abf8c9a78c
PhuCT <phubierd@gmail.com>
2023-03-09 10:33:19 +0700

    handle cart button increse/decrease

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -6,18 +6,47 @@ const defaultCartState = {
     totalAmount: 0
 }
 
-const cartReducer = (state, action) => {
+const cartReducer = (state = defaultCartState, action) => {
     if (action.type === 'ADD') {
-        console.log('go this ???')
-        const updatedItems = state.items.concat(action.item)
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id)
+        const exsitingCartItem = state.items[existingCartItemIndex]
+        let updatedItems;
+
+        if (exsitingCartItem) {
+            const updatedItem = {
+                ...exsitingCartItem,
+                amount: exsitingCartItem.amount + action.item.amount
+            }
+            updatedItems = [...state.items]
+            updatedItems[existingCartItemIndex] = updatedItem
+        } else {
+            updatedItems = state.items.concat(action.item)
+        }
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
         }
     }
     if (action.type === 'REMOVE') {
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.id)
 
+        const existingItem = state.items[existingCartItemIndex]
+        const updatedTotalAmount = state.totalAmount - existingItem.price
+        let updatedItems;
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id)
+        } else {
+            const updatedItem = { ...existingItem, amount: existingItem.amount - 1 }
+            updatedItems = [...state.items]
+            updatedItems[existingCartItemIndex] = updatedItem
+        }
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
     }
     return defaultCartState
 }
